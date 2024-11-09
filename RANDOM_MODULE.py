8250bab0072c29f2cf2b5d9cc9f86002a04d35be
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-11-09 21:26:38 +0530

    Add files via upload

---
+++ RANDOM_MODULE.py
@@ -0,0 +1,18 @@
+# RANDOM MODULE
+
+import random
+
+random_integers = random.randint(1, 10)
+print(random_integers)
+
+random_number_0_to_1 = random.random() * 10
+print(random_number_0_to_1)
+
+random_float = random.uniform(1, 10)
+print(random_float)
+
+random_heads_or_tails = random.randint(0,1)
+if random_heads_or_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
